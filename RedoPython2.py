0b415289deea3229a2a99085ec8039f929bdb1a4
Ralph Cyrel B. Ronda <159085483+ralphcyrel@users.noreply.github.com>
2024-03-23 22:40:19 +0800

    RedoPython2.py

---
+++ RedoPython2.py
@@ -0,0 +1,5 @@
+#the color of the text
+# will ask the user to input his/her name
+# will ask the user to input his/her name
+# will ask the user to input his/her address
+# will ask the user to input his/her email address
