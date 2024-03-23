2adef9b1b6d27d0f0cf86d5aba04a0c782dcd50a
Ralph Cyrel B. Ronda <159085483+ralphcyrel@users.noreply.github.com>
2024-03-23 22:42:42 +0800

    RedoPython2.py

--- RedoPython2.py
+++ RedoPython2.py
@@ -1,4 +1,17 @@
 #the color of the text
+def print_color(text, color):
+    colors = {
+        'red': '\033[31m',
+        'green': '\033[32m',
+        'yellow': '\033[33m',
+        'blue': '\033[34m',
+        'magenta': '\033[35m',
+        'cyan': '\033[36m',
+        'white': '\033[37m',
+    }
+    end_color = '\033[0m'
+    print_color = f"{colors[color]}{'*' * len(text)}\n{text}\n{'*' * len(text)}{end_color}"
+    print(print_color)
 # will ask the user to input his/her name
 # will ask the user to input his/her name
 # will ask the user to input his/her address
